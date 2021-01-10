f106d32010eddc799d5c981dd615bb3a7405f39e
kaanyagci <kaan.yagci@clique.paris>
2021-01-10 03:27:29 +0100

    Parser class updated

--- src/main/java/io/makepad/mxml/parser/Parser.java
+++ src/main/java/io/makepad/mxml/parser/Parser.java
@@ -1,24 +1,42 @@
 package io.makepad.mxml.parser;
+
 import io.makepad.mxml.resolver.MXMLEntityResolver;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
-import java.io.File;
-import java.io.IOException;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.ParserConfigurationException;
-
 public class Parser {
-    public Document document;
-    public Parser(String filePath) throws ParserConfigurationException, IOException, SAXException {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-       // factory.setValidating(true);
-        //factory.setIgnoringComments(true);
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        builder.setEntityResolver(new MXMLEntityResolver());
-        File file = new File(filePath);
-        // TODO: Check for the score-partwise tag to be sure that it is a MusicXML file
-        this.document = builder.parse(file);
+  private static final Logger LOGGER = Logger.getLogger(Parser.class.getName());
+  public Document document;
+
+  public Parser(String filePath) {
+    try {
+      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+      factory.setValidating(true);
+      factory.setIgnoringComments(true);
+      DocumentBuilder builder = factory.newDocumentBuilder();
+      builder.setEntityResolver(new MXMLEntityResolver());
+      File file = new File(filePath);
+      System.out.println("New file created");
+      this.document = builder.parse(file);
+    } catch (ParserConfigurationException e) {
+      LOGGER.throwing(Parser.class.getName(), "constructor", e);
+      e.printStackTrace();
+    } catch (IOException e) {
+      LOGGER.throwing(Parser.class.getName(), "construcor", e);
+      e.printStackTrace();
+    } catch (SAXException e) {
+      LOGGER.throwing(Parser.class.getName(), "construcor", e);
+      e.printStackTrace();
     }
+  }
+
+  private boolean checkDocument() {
+      return this.document.getDocumentElement().getTagName().equals("score-partwise");
+  }
 }
