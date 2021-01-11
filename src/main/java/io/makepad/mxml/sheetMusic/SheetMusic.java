27143c96f4d1c51b1660efa47513d5c7ca9488e6
kaanyagci <kaan.yagci@clique.paris>
2021-01-11 01:45:00 +0100

    SheetMusic package created with a new SheetMusic class

---
+++ src/main/java/io/makepad/mxml/sheetMusic/SheetMusic.java
@@ -0,0 +1,14 @@
+package io.makepad.mxml.sheetMusic;
+
+import org.w3c.dom.Document;
+
+public class SheetMusic {
+
+  /*
+  * Class constructor
+  * @param document A document parsed from the given MusicXML file
+  */
+  public SheetMusic(Document document) {
+    // TODO: Complete with document parsing to sheet music
+  }
+}
